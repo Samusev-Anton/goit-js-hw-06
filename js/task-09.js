dd7152d6ee0ebf41832c54fd40cc41588df593ff
Anton <104672550+Samusev-Anton@users.noreply.github.com>
2022-09-12 20:34:11 +0300

    Block HW6

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColor = document.querySelector("body");
+// console.log(bodyColor);
+const textSpan = document.querySelector('.color');
+// console.log(textSpan);
+const btnChangeColor = document.querySelector('.change-color');
+// console.log(btnChangeColor);
+
+
+btnChangeColor.addEventListener('click', onBtnChangeColor);
+
+function onBtnChangeColor() {
+  const color = getRandomHexColor();
+  console.log(color);
+
+  bodyColor.style.backgroundColor = color;
+  textSpan.textContent = color;
+}
+
+
--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,43 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const bntCreateDiv = document.querySelector('button[data-create]');
+// console.log(bntCreateDiv);
+const bntDestroy = document.querySelector('button[data-destroy]');
+// console.log(bntDestroy);
+const randomColor = getRandomHexColor();
+// console.log(randomColor);
+const insertPoint = document.querySelector('#boxes');
+// console.log(insertPoint);
+// const inputEl = document.querySelector('input');
+
+bntCreateDiv.addEventListener('click', createBoxes);
+// inputEl.addEventListener('input', onInputeChange);
+
+// function onInputeChange(event) {
+//   const inputDataStr = Number(event.currentTarget.value);
+//   console.log(inputDataStr);
+// }
+
+
+function createBoxes(amount) {
+  let boxElements = [];
+  let boxHeight = 20;
+  let boxWidht = 20;
+  for (var i = 0; i < amount; i+1) {
+    boxHeight += 10;
+    boxWidht += 10;
+    const divEl = document.createElement('div');
+    divEl.style.height = `${boxHeight}px`;
+    divEl.style.width = `${boxWidht}px`;
+    divEl.style.background = getRandomHexColor();
+    // console.log(divEl);
+    boxElements.push(divEl);
+// код создания элемента
+  }
+  console.log(boxElements);
+  insertPoint.append(...boxElements);
+}
+// createBoxes(7);
